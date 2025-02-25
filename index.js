d141800101d619c5fe83e89258d76978b970bc56
Silongo Vailolo <silongo68@hotmail.com>
2025-02-25 12:07:58 +1000

    Done Looing Lab

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
 // Code your solutions in this file
+
+function writeCards(names, occasion) {
+    return names.map(name => `Thank you, ${name}, for the wonderful ${occasion} gift!`)
+};
+
+function countDown(start) {
+    for (let i = start; i >= 0; i--) {
+        console.log(i);
+    }
+}
